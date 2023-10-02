ab1bccfacdb860a06401b3682f5871f96a0052b6
Pablo Fajardo <pfajardodiaz17@gmail.com>
2023-10-02 14:36:14 -0500

    Setting the backend

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
 # Backend
+terraform {
+    required_version = "~> 1.5"
+    backend "s3" {
+        bucket = "modelsappbucket"
+        key = "infra/terraform.state"
+        region = "us-east-1"
+        profile = "pfajardo"
+        encrypt = true
+    }
+    required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
 
-# Provider
+# Provider region
+provider "aws" {
+  region = "us-east-1"
+  profile = "pfajardo"
+}
 
 # Main file
